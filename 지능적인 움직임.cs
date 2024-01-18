e5c2b4a7eada7b87aa8a655f0d16543a52ddc43c
Moses Tiger <150508386+MosesTiger@users.noreply.github.com>
2024-01-18 17:23:49 +0900

    Add files via upload

---
+++ 지능적인 움직임.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Specialized;
+
+public class Agent: MonoBehaviour
+{
+    public float maxSpeed;
+    public float maxAccel;
+    public float orientation;
+    public float ratation;
+    public Vector3 velocity;
+
+    protected Steering steering;
+
+    void Start()
+    {
+        velocity = vector3.zero;
+        steering = new Steering();
+    }
+
+    public void SetSteering(Steering steering)
+    {
+        this.steering = steering;
+    }
+}
