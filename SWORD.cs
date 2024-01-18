a83c5379e8bb800e1a64ad17448a455511ae6024
Moses Tiger <150508386+MosesTiger@users.noreply.github.com>
2024-01-18 17:26:18 +0900

    Add files via upload

---
+++ SWORD.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWORD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
